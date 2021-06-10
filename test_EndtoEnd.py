56169958bc49450bdff7f5d19eff628809973882
bharathkunarv <direct2bharathkumar@gmail.com>
2021-06-11 01:08:56 +0530

    Added Allure Report

--- test_EndtoEnd.py
+++ test_EndtoEnd.py
@@ -9,10 +9,12 @@ from GreenKartEndtoEnd.TestData.HomePageData import HomePageData
 
 class Test_End_to_End(BaseClass):
     def test_End_to_End(self, data_load):
-
+        log = self.getLogger()
         # self.driver.find_element_by_css_selector("input.search-keyword").send_keys("ber")
         Home_Page = HomePage(self.driver)
+        log.info("Homepage loaded")
         Home_Page.search_bar().send_keys(data_load["vegetable_name"])
+        log.info("Searching veggies" + " " + data_load["vegetable_name"])
         # self.driver.find_element_by_css_selector("button.search-button").click()
         Home_Page.search_icon().click()
         time.sleep(3)
@@ -26,11 +28,13 @@ class Test_End_to_End(BaseClass):
         Home_Page.cart_button().click()
         # self.driver.find_element_by_xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]").click()
         Checkout_Page = Home_Page.proceed_to_checkout()
+        log.info("Moving to checkout page")
 
         # CheckoutPage
         time.sleep(3)
         # self.driver.find_element_by_css_selector("input.promoCode").send_keys("rahulshettyacademy")
         Checkout_Page.promo_code().send_keys(data_load["promocode"])
+        log.info("entered promocode")
 
         # self.driver.find_element_by_css_selector("button.promoBtn").click()
         Checkout_Page.promo_button().click()
@@ -46,7 +50,7 @@ class Test_End_to_End(BaseClass):
         for country in countries:
             if country.text == data_load["country_name"]:
                 country.click()
-
+        log.info("Country selected")
         # self.driver.find_element_by_css_selector("input.chkAgree").click()
         Confirmation_page.check_box().click()
         # self.driver.find_element_by_xpath("//button[contains(text(),'Proceed')]").click()
